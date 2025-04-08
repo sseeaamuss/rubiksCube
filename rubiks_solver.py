9e7d2088bbd1f95aa5fdb58a57d736040795eea9
Seamus <smo197@uclive.ac.nz>
2025-04-09 09:40:29 +1200

    initial commit

---
+++ rubiks_solver.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This is my rubik's cube software for Engineering Optimisation ENEL445
+This project will build upon a 2*2*2 rubik's cube and work up to a 3*3*3 cube 
+if successful 
+
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+def plot_cube:
+    """This function plots the 2d unfolded layout of the 2*2*2 cube"""
